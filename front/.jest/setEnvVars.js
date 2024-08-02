40904be5e1d8907098d2b488e31716d40178c664
Gass <madebygass@gmail.com>
2024-08-02 11:42:48 +0200

    clean repo

--- front/.jest/setEnvVars.js
+++
@@ -1,2 +0,0 @@
-process.env.REACT_APP_SERVER_PORT = 3000;
-process.env.REACT_APP_SERVER_URL = "https://register-base-backend.vercel.app";
--- server/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-const dotenv = require("dotenv");
-dotenv.config();
-
-app.listen(8000, () => {
-  console.log("server running on port ${8000}");
-});
