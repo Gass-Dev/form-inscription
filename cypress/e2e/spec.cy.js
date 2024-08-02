9dfe18c14f6c1474d03d05b00145e1e5fa4ae0b0
Gass <madebygass@gmail.com>
2024-08-02 12:22:31 +0200

    chnage test cypress e2e

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,28 @@
 /* eslint-disable no-undef */
 describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-  });
-
   it("deployed react app to localhost", () => {
+    cy.intercept("GET", "https://register-base-backend.vercel.app", {
+      statusCode: 200,
+      body: {
+        utilisateurs: [
+          {
+            id: "1",
+            name: "a",
+            email: "a@mail.com",
+          },
+          {
+            id: "2",
+            name: "b",
+            email: "b@mail.com",
+          },
+        ],
+      },
+    });
+
+    // Visit the local React app
     cy.visit("http://localhost:3000");
-    cy.contains("1 user(s) already registered");
+
+    // Assert that the correct text is displayed
+    cy.contains("2 user(s) already registered");
   });
 });
